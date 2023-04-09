7c3226998326e23b4bcfcccbb87d530e0b45cfd7
Beverly M <bevponi@gmail.com>
2023-04-09 18:02:54 +0000

    falta boton purpura Switch

---
+++ src/js/component/Button2.js
@@ -0,0 +1,14 @@
+import Button from '@mat'
+
+function Button2() {
+    return (
+        <div>
+            <Button color="purple"
+                variant='outlined'>
+                Púrpura
+            </Button>
+
+        </div>
+    )
+}
+export default Button2;
