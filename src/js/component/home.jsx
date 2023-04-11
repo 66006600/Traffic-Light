a2641aa8f244bffa7f54306c9cd0ccdae3b760b4
Beverly M <bevponi@gmail.com>
2023-04-11 02:29:53 +0000

    Terminadoooooo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,13 +4,9 @@ import Button from "./Button.js";
 //create your first component
 const Home = () => {
     const [selectedColor, setSelectedColor] = useState("red");
-
-    const [purple, setPurple] = useState(false) 
-
+    const [purple, setPurple] = useState(false);
     const changeToPurple = () => {
-        setPurple(true) ? "purple" : "red";
-        
-
+        setPurple(purple => !purple);
     };
 
     return (
@@ -19,16 +15,18 @@ const Home = () => {
             <div className="traffic-Light">
 
                 <div onClick={() => setSelectedColor("red")}
-                    className={"light " + ((purple === true) ? "purple" : "red") + ((selectedColor === "red") ? " glow" : "")}> </div>
+                    className={"light " + ((purple === true) ? "purple" : "red") + ((selectedColor === "red") ? " glow-animation" : "")}> </div>
 
                 <div onClick={() => setSelectedColor("yellow")}
-                    className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}> </div>
+                    className={"light yellow" + ((selectedColor === "yellow") ? " glow-animation" : "")}> </div>
 
                 <div onClick={() => setSelectedColor("green")}
-                    className={"light green" + ((selectedColor === "green") ? " glow" : "")}> </div>
+                    className={"light green" + ((selectedColor === "green") ? " glow-animation" : "")}> </div>
                 <br></br>
 
                 <div className="btn" onClick={changeToPurple}>Purple</div>
+                {purple ? "Red" : "Purple"}
+
             </div>
         </div>
     );
